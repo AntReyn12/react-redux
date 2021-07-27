9a817ac06f6565b7731da592554a840b52b1c564
ant_reyn <antreyn@udel.edu>
2021-07-27 17:17:09 -0400

    created counter app using redux for different components

---
+++ src/Counter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment} from "./redux/ducks/counter";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  return (
+    <div style={{backgroundColor: "black"}}>
+      <div style={{margin: "10px"}}>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
